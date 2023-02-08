03725792484f87b7fc040bee662ebcb25130e4a7
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-08 11:40:43 +0100

    numbers now can be added to the screen

--- Swift-Calculadora/ViewController.swift
+++ Swift-Calculadora/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelNbr: UITextField!
     var actualNbr: String = "0";
     var prevNbr: String? = nil;
+    
+    let MAX_NBR_SIZE = 9;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,22 +23,56 @@ class ViewController: UIViewController {
 
     @IBAction func onNumberClicked(_ sender: UIButton) {
         let nbr: String? = sender.titleLabel?.text; // Not nil
+        append(nbr!)
+        updateUI();
+    }
+    
+    @IBAction func onCommaClicked(_ sender: UIButton) {
+        let nbr: String? = sender.titleLabel?.text; // Not nil
+        append(nbr!)
+        updateUI();
+    }
+    
+    func append(_ c: String) {
+        // Appends to the right the given character
+        if actualNbr.count >= MAX_NBR_SIZE {
+            return
+        }
+        if c == "," && actualNbr.contains(c) { // Check , already there
+            return
+        }
         
         if actualNbr == "0" {
-            actualNbr = nbr!;
+            if c == "," {
+                actualNbr += c
+            }
+            else {
+                actualNbr = c
+            }
         }
         else {
-            actualNbr = actualNbr + nbr!;
+            actualNbr += c
         }
-        
-        labelNbr.text = actualNbr;
+    }
+    
+    @IBAction func onDeleteClicked(_ sender: UIButton) {
+        if actualNbr.count == 1 {
+            actualNbr = "0"
+        }
+        else {
+            actualNbr.removeLast()
+        }
+        updateUI()
     }
     
     @IBAction func onClearClicked(_ sender: UIButton) {
         actualNbr = "0";
-        
-        labelNbr.text = actualNbr;
+        updateUI();
     }
     
+    func updateUI() {
+        // TODO dot format?
+        labelNbr.text = actualNbr;
+    }
 }
 

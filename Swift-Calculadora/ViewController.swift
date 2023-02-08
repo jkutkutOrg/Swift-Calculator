8f3c96d4883983ea5b8443839fcd02551edb14cb
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-08 10:49:56 +0100

    numbers now added to the screen

--- Swift-Calculadora/ViewController.swift
+++ Swift-Calculadora/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelNbr: UITextField!
+    var actualNbr: String = "0";
+    var prevNbr: String? = nil;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +19,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onNumberClicked(_ sender: UIButton) {
+        let nbr: String? = sender.titleLabel?.text; // Not nil
+        
+        if actualNbr == "0" {
+            actualNbr = nbr!;
+        }
+        else {
+            actualNbr = actualNbr + nbr!;
+        }
+        
+        labelNbr.text = actualNbr;
+    }
+    
+    @IBAction func onClearClicked(_ sender: UIButton) {
+        actualNbr = "0";
+        
+        labelNbr.text = actualNbr;
+    }
+    
 }
 
